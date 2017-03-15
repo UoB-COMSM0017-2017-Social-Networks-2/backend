2007b003a4bef231eaed42a2f06cfa79d4fea3b2
Floris Kint <floris.kint@gmail.com>
2017-03-15 12:28:18 +0000

    Set logging level lower

--- main.py
+++ main.py
@@ -6,18 +6,20 @@ import mining
 
 app = Flask("SN2", template_folder='templates')
 app.config.from_pyfile('config.cfg')
+logging.basicConfig(level=logging.DEBUG)
 
 from flask_oauthlib.client import OAuth
 
 oauth = OAuth()
-twitter = oauth.remote_app('twitter',
-                           base_url='https://api.twitter.com/1/',
-                           request_token_url='https://api.twitter.com/oauth/request_token',
-                           access_token_url='https://api.twitter.com/oauth/access_token',
-                           authorize_url='https://api.twitter.com/oauth/authenticate',
-                           consumer_key=app.config['TWITTER_CONSUMER_KEY'],
-                           consumer_secret=app.config['TWITTER_CONSUMER_SECRET'],
-                           )
+twitter = oauth.remote_app(
+    'twitter',
+    base_url='https://api.twitter.com/1/',
+    request_token_url='https://api.twitter.com/oauth/request_token',
+    access_token_url='https://api.twitter.com/oauth/access_token',
+    authorize_url='https://api.twitter.com/oauth/authenticate',
+    consumer_key=app.config['TWITTER_CONSUMER_KEY'],
+    consumer_secret=app.config['TWITTER_CONSUMER_SECRET'],
+)
 
 
 @twitter.tokengetter
