e25874110cce10342d2bd0f5a7003fcb769afb92
Floris Kint <floris.kint@gmail.com>
2017-03-28 15:26:25 +0100

    Add template for actual application

--- views/__init__.py
+++ views/__init__.py
@@ -8,3 +8,8 @@ from views.data import *
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/application')
+def application():
+    return render_template('application.html')
